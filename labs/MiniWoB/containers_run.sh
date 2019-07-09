b23df1b8207c1ca85d9d6e8c14868df155766fb1
Max Lapan <max.lapan@gmail.com>
2019-07-09 16:02:51 +0300

    Taxonomy section for intro

--- labs/MiniWoB/containers_run.sh
+++ labs/MiniWoB/containers_run.sh
@@ -12,5 +12,5 @@ for i in `seq 1 ${1:-1}`; do
     echo Starting container $i
     P1=$((5900+$i-1))
     P2=$((15900+$i-1))
-    docker run -d -p $P1:5900 -p $P2:15900 --privileged --ipc host --cap-add SYS_ADMIN $IMAGE_NAME run
+    docker run -d -p $P1:5900 -p $P2:15900 --privileged --ipc host --cap-add SYS_ADMIN $IMAGE_NAME run -f 5
 done
